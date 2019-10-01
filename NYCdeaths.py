c3d9d6a84624430cd96cf9b525d73501c4d55ded
jhjhjhsu <54126309+jhjhjhsu@users.noreply.github.com>
2019-09-30 20:31:05 -0400

    Update NYCdeaths.py

--- NYCdeaths.py
+++ NYCdeaths.py
@@ -36,9 +36,9 @@ year = input("Which year would you like to look up from 2007-2014? ")
 year = str(year)
 sex = input ("Which sex would you like to look up? (1=Male, 2= Female) ")
 if sex == "1":
-    sex = "Male"
+    sex = "M"
 elif sex == "2":
-    sex = "Female"
+    sex = "F"
 else:
     print (error) #need edits
 print()
@@ -67,22 +67,21 @@ except UnicodeError as error:
 
 fileFromString = io.StringIO(s)
 lines = csv.reader(fileFromString)   #or lines = csv.reader(s.splitlines())
-SelectedYear = [line for line in lines if line[0] == year] #select year of choice
+selectedYear = [line for line in lines if line[0] == year] #select year of choice
 fileFromString.close()
 
 #filter data by sex and ethnicity
-FilteredList = []
-for x in SelectedYear:
-   if SelectedYear[2] == sex and SelectedYear[3] == ethnicity:
-        FilteredList.append(x)
-FilteredList.sort(key = FilteredList[4])
+filteredList = []
+for x in selectedYear:
+   if x[2] == sex and x[3] == ethnicity:
+        filteredList.append(x)
+filteredList.sort(key = lambda x: x[4])
         
 
 #print leading causes of death
 #need to print header
 print (f"In {year}, the leading causes of death for {ethnicity} {sex} in New York City are:")
-for FilteredList in range (11):
-    print (FilteredList[2], FilteredList[4])
-
-
+for x in filteredList[:10]:
+    print(x[2], x[4])
+    
 sys.exit(0)
